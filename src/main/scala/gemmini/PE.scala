6ab018c78e84f573f3dd2f3eef61cc2028d12144
Eric Dong <ericdong@g.harvard.edu>
2022-11-29 21:43:41 -0500

    comment out original PE

--- src/main/scala/gemmini/PE.scala
+++ src/main/scala/gemmini/PE.scala
@@ -12,44 +12,8 @@ class PEControl[T <: Data : Arithmetic](accType: T) extends Bundle {
 
 }
 
-// class PE_BLACKBOX[T <: Data](inputType: T, outputType: T, accType: T, df: Dataflow.Value, max_simultaneous_matmuls: Int)
-//                    (implicit ev: Arithmetic[T]) extends BlackBox with HasBlackBoxResource {
-//   import ev._
-
-//   val io = IO(new Bundle {
-//     val in_a = Input(inputType)
-//     val in_b = Input(outputType)
-//     val in_d = Input(outputType)
-//     val out_a = Output(inputType)
-//     val out_b = Output(outputType)
-//     val out_c = Output(outputType)
-
-//     val in_control = Input(new PEControl(accType))
-//     val out_control = Output(new PEControl(accType))
-
-//     val in_id = Input(UInt(log2Up(max_simultaneous_matmuls).W))
-//     val out_id = Output(UInt(log2Up(max_simultaneous_matmuls).W))
-
-//     val in_last = Input(Bool())
-//     val out_last = Output(Bool())
-
-//     val in_valid = Input(Bool())
-//     val out_valid = Output(Bool())
-
-//     val bad_dataflow = Output(Bool())
-//   })
-
-//   addResource("./vsrc/full_mul.sv")
-
-// }
-
-// TODO update documentation
-/**
-  * A PE implementing a MAC operation. Configured as fully combinational when integrated into a Mesh.
-  * @param width Data width of operands
-  */
 class PE_BLACKBOX[T <: Data](inputType: T, outputType: T, accType: T, df: Dataflow.Value, max_simultaneous_matmuls: Int)
-                   (implicit ev: Arithmetic[T]) extends Module { // Debugging variables
+                   (implicit ev: Arithmetic[T]) extends BlackBox with HasBlackBoxResource {
   import ev._
 
   val io = IO(new Bundle {
@@ -75,72 +39,108 @@ class PE_BLACKBOX[T <: Data](inputType: T, outputType: T, accType: T, df: Datafl
     val bad_dataflow = Output(Bool())
   })
 
-  val cType = if (df == Dataflow.WS) inputType else accType
+  addResource("./vsrc/full_mul.sv")
 
-  val a  = io.in_a
-  val b  = io.in_b
-  val d  = io.in_d
-  val c1 = Reg(cType)
-  val c2 = Reg(cType)
-  val dataflow = io.in_control.dataflow
-  val prop  = io.in_control.propagate
-  val shift = io.in_control.shift
-  val id = io.in_id
-  val last = io.in_last
-  val valid = io.in_valid
-
-  io.out_a := a
-  io.out_control.dataflow := dataflow
-  io.out_control.propagate := prop
-  io.out_control.shift := shift
-  io.out_id := id
-  io.out_last := last
-  io.out_valid := valid
-
-  val last_s = RegEnable(prop, valid)
-  val flip = last_s =/= prop
-  val shift_offset = Mux(flip, shift, 0.U)
-
-  // Which dataflow are we using?
-  val OUTPUT_STATIONARY = Dataflow.OS.id.U(1.W)
-  val WEIGHT_STATIONARY = Dataflow.WS.id.U(1.W)
-
-  // Is c1 being computed on, or propagated forward (in the output-stationary dataflow)?
-  val COMPUTE = 0.U(1.W)
-  val PROPAGATE = 1.U(1.W)
-
-  io.bad_dataflow := false.B
-  when ((df == Dataflow.OS).B || ((df == Dataflow.BOTH).B && dataflow === OUTPUT_STATIONARY)) {
-    when(prop === PROPAGATE) {
-      io.out_c := (c1 >> shift_offset).clippedToWidthOf(outputType)
-      io.out_b := b
-      c2 := c2.two_mac(a, b.asTypeOf(inputType)) // in OS mode c2 holds the output and acculumates to it
-      c1 := d.withWidthOf(cType)
-    }.otherwise {
-      io.out_c := (c2 >> shift_offset).clippedToWidthOf(outputType)
-      io.out_b := b
-      c1 := c1.two_mac(a, b.asTypeOf(inputType))
-      c2 := d.withWidthOf(cType)
-    }
-  }.elsewhen ((df == Dataflow.WS).B || ((df == Dataflow.BOTH).B && dataflow === WEIGHT_STATIONARY)) {
-    when(prop === PROPAGATE) {
-      io.out_c := c1
-      io.out_b := b.two_mac(a, c2.asTypeOf(inputType)) // in WS mode, c2 holds the weight, b is the accumulated result
-      c1 := d
-    }.otherwise {
-      io.out_c := c2
-      io.out_b := b.two_mac(a, c1.asTypeOf(inputType))
-      c2 := d
-    }
-  }.otherwise {
-    io.bad_dataflow := true.B
-    //assert(false.B, "unknown dataflow")
-    io.out_c := DontCare
-    io.out_b := DontCare
-  }
-
-  when (!valid) {
-    c1 := c1
-    c2 := c2
-  }
 }
+
+// // TODO update documentation
+// /**
+//   * A PE implementing a MAC operation. Configured as fully combinational when integrated into a Mesh.
+//   * @param width Data width of operands
+//   */
+// class PE_BLACKBOX[T <: Data](inputType: T, outputType: T, accType: T, df: Dataflow.Value, max_simultaneous_matmuls: Int)
+//                    (implicit ev: Arithmetic[T]) extends Module { // Debugging variables
+//   import ev._
+
+//   val io = IO(new Bundle {
+//     val in_a = Input(inputType)
+//     val in_b = Input(outputType)
+//     val in_d = Input(outputType)
+//     val out_a = Output(inputType)
+//     val out_b = Output(outputType)
+//     val out_c = Output(outputType)
+
+//     val in_control = Input(new PEControl(accType))
+//     val out_control = Output(new PEControl(accType))
+
+//     val in_id = Input(UInt(log2Up(max_simultaneous_matmuls).W))
+//     val out_id = Output(UInt(log2Up(max_simultaneous_matmuls).W))
+
+//     val in_last = Input(Bool())
+//     val out_last = Output(Bool())
+
+//     val in_valid = Input(Bool())
+//     val out_valid = Output(Bool())
+
+//     val bad_dataflow = Output(Bool())
+//   })
+
+//   val cType = if (df == Dataflow.WS) inputType else accType
+
+//   val a  = io.in_a
+//   val b  = io.in_b
+//   val d  = io.in_d
+//   val c1 = Reg(cType)
+//   val c2 = Reg(cType)
+//   val dataflow = io.in_control.dataflow
+//   val prop  = io.in_control.propagate
+//   val shift = io.in_control.shift
+//   val id = io.in_id
+//   val last = io.in_last
+//   val valid = io.in_valid
+
+//   io.out_a := a
+//   io.out_control.dataflow := dataflow
+//   io.out_control.propagate := prop
+//   io.out_control.shift := shift
+//   io.out_id := id
+//   io.out_last := last
+//   io.out_valid := valid
+
+//   val last_s = RegEnable(prop, valid)
+//   val flip = last_s =/= prop
+//   val shift_offset = Mux(flip, shift, 0.U)
+
+//   // Which dataflow are we using?
+//   val OUTPUT_STATIONARY = Dataflow.OS.id.U(1.W)
+//   val WEIGHT_STATIONARY = Dataflow.WS.id.U(1.W)
+
+//   // Is c1 being computed on, or propagated forward (in the output-stationary dataflow)?
+//   val COMPUTE = 0.U(1.W)
+//   val PROPAGATE = 1.U(1.W)
+
+//   io.bad_dataflow := false.B
+//   when ((df == Dataflow.OS).B || ((df == Dataflow.BOTH).B && dataflow === OUTPUT_STATIONARY)) {
+//     when(prop === PROPAGATE) {
+//       io.out_c := (c1 >> shift_offset).clippedToWidthOf(outputType)
+//       io.out_b := b
+//       c2 := c2.two_mac(a, b.asTypeOf(inputType)) // in OS mode c2 holds the output and acculumates to it
+//       c1 := d.withWidthOf(cType)
+//     }.otherwise {
+//       io.out_c := (c2 >> shift_offset).clippedToWidthOf(outputType)
+//       io.out_b := b
+//       c1 := c1.two_mac(a, b.asTypeOf(inputType))
+//       c2 := d.withWidthOf(cType)
+//     }
+//   }.elsewhen ((df == Dataflow.WS).B || ((df == Dataflow.BOTH).B && dataflow === WEIGHT_STATIONARY)) {
+//     when(prop === PROPAGATE) {
+//       io.out_c := c1
+//       io.out_b := b.two_mac(a, c2.asTypeOf(inputType)) // in WS mode, c2 holds the weight, b is the accumulated result
+//       c1 := d
+//     }.otherwise {
+//       io.out_c := c2
+//       io.out_b := b.two_mac(a, c1.asTypeOf(inputType))
+//       c2 := d
+//     }
+//   }.otherwise {
+//     io.bad_dataflow := true.B
+//     //assert(false.B, "unknown dataflow")
+//     io.out_c := DontCare
+//     io.out_b := DontCare
+//   }
+
+//   when (!valid) {
+//     c1 := c1
+//     c2 := c2
+//   }
+// }
